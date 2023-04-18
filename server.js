def75113183d6fd7d7cb252eeccfd0b76f352b81
rithchanvirakboth <rithboth0@gmail.com>
2023-04-18 10:37:33 +0700

    [initialized]: initialize server side

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+}, (err) => {
+  console.log(err);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`app listening on port: ${port}`);
+});
+
