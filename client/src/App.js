037b57dd82dd80861989af936e7d7cdc10904dcb
rithchanvirakboth <rithboth0@gmail.com>
2023-04-23 15:58:10 +0700

    [Frontend](src): apply all dependency, css, bootstrap and font into frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* Navbar */}
+        <Navbar />
+
+        {/* content */}
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+
+        {/* Footer */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+         {/* Logo big screen and bars smaller screen */}
+
+         {/* navbar items big screen and logo smaller screen */}
+
+         {/* Profile Dropdown */}
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
