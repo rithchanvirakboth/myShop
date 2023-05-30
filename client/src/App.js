ec003539db1e104a704a0f2d3632cf0e2757b133
rithchanvirakboth <rithboth0@gmail.com>
2023-05-30 15:51:50 +0700

    [frontend]: initialized frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,24 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import Jumbotron from "./components/Jumbotron/Jumbotron";
-import Content from "./components/content/content";
-import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
-import { dispatchGetUser, dispatchLogin, fetchUser } from "./redux/actions/authAction";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const dispatch = useDispatch();
-  const token = useSelector((state) => state.token);
-  const auth = useSelector((state) => state.auth);
-
-  useEffect(() => {
-    const firstLogin = localStorage.getItem("firstLogin");
-    if (firstLogin) {
-      const getToken = async () => {
-        const res = await axios.post("/user/refresh_token", null);
-        dispatch({ type: "GET_TOKEN", payload: res.data.access_token });
-      };
-      getToken();
-    }
-  }, [auth.isLogged, dispatch]);
-
-  useEffect(() => {
-    if (token) {
-      const getUser = () => {
-        dispatch(dispatchLogin());
-
-        return fetchUser(token).then((res) => {
-          dispatch(dispatchGetUser(res));
-        });
-      };
-      getUser();
-    }
-  }, [token, dispatch]);
-
   return (
-    <Router>
-      <div>
-        {/* Navbar */}
-        <div className="sticky-top">
-          <Navbar />
-        </div>
-
-        {/* Content */}
-        <Content />
-
-        {/* Jumbotron */}
-        <Jumbotron />
-        {/* Footer */}
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- client/src/features/Menu/Our-store/Our-store.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function OurStore() {
-  return (
-    <div>OurStore</div>
-  )
-}
-
-export default OurStore
